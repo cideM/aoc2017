6f8a5cca5468d318d53a12e99ca3513a0f2a894c
Florian Beeres <yuuki@protonmail.com>
2023-11-18 19:05:49 +0100

    Simplify D1 again

--- d1/main.lua
+++ d1/main.lua
@@ -1,43 +1,19 @@
-local CircularList = {}
-
-function CircularList:new(o)
-	o = o or {}
-	setmetatable(o, self)
-	self.__index = function(t, k)
-		if type(k) ~= "number" then
-			return rawget(self, k)
-		end
-		local rem = k % #t
-		return rem == 0 and t[#t] or t[rem]
-	end
-	return o
-end
-
-function CircularList:iter()
-	local len = #self
-	local i = 0
-	return function()
-		i = i + 1
-		if i > len then
-			return nil, nil
-		end
-		local v = rawget(self, i)
-		return i, v
+local nums, sum, sum2 = {}, 0, 0
+for line in io.lines() do
+	for c in line:gmatch(".") do
+		table.insert(nums, tonumber(c))
 	end
 end
 
-local nums = CircularList:new()
-for c in io.read("a"):gmatch(".") do
-	table.insert(nums, tonumber(c))
-end
+for i, c in ipairs(nums) do
+	local rem = (i - 1) % #nums -- p1
+	if c == nums[rem == 0 and #nums or rem % #nums] then
+		sum = sum + c
+	end
 
-local sum_1, sum_2 = 0, 0
-for i, c in nums:iter() do
-	if c == nums[i - 1] then
-		sum_2 = sum_2 + c
-	end
-	if c == nums[i + #nums / 2] then
-		sum_1 = sum_1 + c
+	rem = (i + #nums / 2) -- p2
+	if c == nums[rem == 0 and #nums or rem % #nums] then
+		sum2 = sum2 + c
 	end
 end
-print(sum_1, sum_2)
+print(sum, sum2)
